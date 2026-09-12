ed5c10e98bce1f840ef0ea4dc65df0a20ed43f11
Aman Patel <a24032002p2@gmail.com>
2026-09-12 21:46:14 +0530

    Time: 787 ms (8.01%), Space: 203.2 MB (25.22%) - LeetHub

---
+++ 4051-count-subarrays-with-distant-sums/4051-count-subarrays-with-distant-sums.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef __int128_t lll;
+#define UM unordered_map
+#define US unordered_set
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define For(i,a,b) for (int i = a; i < b; i++)
+
+
+class Solution {
+public:
+    #define ll long long
+    typedef tree<pair<ll, int>, null_type, less<pair<ll, int>>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+    
+    long long distantSubarrays(vector<int>& nums, int goal, int k) {
+        int n = nums.size();
+        vector<ll> pref(n);
+        for(int i = 0; i < n; i++) {
+            if(i == 0) pref[i] = nums[i];
+            else pref[i] = pref[i-1] + (ll)nums[i];
+        }
+        
+        ordered_set s;
+        for(int i = 0; i < n; i++) {
+            s.insert({pref[i], i});
+        }
+        
+        ll res = 0;
+        for(int i = 0; i < n; i++) {
+            ll last = 0;
+            if(i > 0) last = pref[i-1];
+            
+            if(i != 0){
+                s.erase({last, i-1}); 
+            }
+            
+            ll left = last + goal - k;
+            ll right = last + goal + k;
+            ll leftCnt = s.order_of_key({left, n + 1});
+            ll rightCnt = s.order_of_key({right, -1});
+            ll middle = max((ll)0, rightCnt - leftCnt);
+    
+            res += s.size() - middle;
+        }
+        return res;
+    }
+};
